b8d4a30cbb1eb5809157011b24c2e36d6a2934c8
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-03-14 11:57:28 +0500

    vote html updated

--- vote/vote_app/urls.py
+++ vote/vote_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'vote_app'
 
 urlpatterns = [
-    path('', views.home_view, name='home')
+    path('', views.home_view, name='home'),
+    path('vote/', views.vote_view, name='vote')
 ]
--- vote/vote_app/views.py
+++ vote/vote_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'index.html')
+
+def vote_view(request):
+    return render(request, 'vote.html')
